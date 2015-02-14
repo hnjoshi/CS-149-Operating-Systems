1cbb1f3e3978db0b9244a80450a50c154f35c8ad
hnjoshi <hnjoshi@live.com>
2015-02-13 21:52:22 -0800

    Shortest Job First Complete
    Based on sorted Arrival Time

--- ProcessScheduling/src/ShortestJobFirst.java
+++ ProcessScheduling/src/ShortestJobFirst.java
@@ -4,32 +4,90 @@ import java.util.Comparator;
 
 /**
  * This class runs Shortest job First Algorithm.
- * @author Kernal
+ * @author Kernel
  */
 public class ShortestJobFirst {
+	
+	Process p,p2 = null;
+	int counter = 0, runTime = 0, holdOn = 2;
 	
 	ArrayList<Process> processes; 
 	public ShortestJobFirst(ArrayList<Process> p) {
 		processes = p;
-		
-		//Sort based on Runtime, if the arraival time is same. 
-		Collections.sort(processes, new Comparator<Process>() {
-			@Override
-			public int compare(Process p1, Process p2) {
-				int comp = Float.compare(p1.getArrivalTime(), p2.getArrivalTime());
-				if(comp == 0) {
-					comp = Float.compare(p1.getRunTime(), p2.getRunTime());
-				}
-				return comp;
-			}			
-		}); 
 	}
 	
 	//This function performs the Shortest Job First Algorithm. 
 	public ArrayList<Process> sjf() {
 		ArrayList<Process> result = new ArrayList<Process>();
+		int Total_Processes = processes.size();
 		
-		//VADIL your work goes here! 
+		for(Process p : processes) {
+			System.out.println(p);
+		}
+		
+		boolean isFull = false;
+		while(!isFull) {
+			
+			if(counter==0)
+			{
+				p = processes.get(counter);
+				while(p.getRunTime() > 0) {
+					result.add(p);
+					p.setRunTime(p.getRunTime() - 1);
+					runTime++;
+				}
+				counter++;
+			}
+			else
+			{
+				if(counter < Total_Processes){
+					p = processes.get(counter);
+				}
+				
+				if(holdOn < Total_Processes){
+					p2 = processes.get(holdOn);
+				}
+				
+				if(p.getRunTime() < p2.getRunTime())
+				{
+					while(p.getRunTime() > 0) {
+						result.add(p);
+						p.setRunTime(p.getRunTime() - 1);
+						runTime++;
+					}
+					if(counter < holdOn) {
+						counter = holdOn;
+					} else {
+						counter++;
+					}
+					holdOn++;
+				}
+				else
+				{
+					while(p2.getRunTime() > 0) {
+						result.add(p2);
+						p2.setRunTime(p2.getRunTime() - 1);
+						runTime++;
+					}
+					holdOn++;
+				}	
+			}
+			
+			if(processes.get(Total_Processes-1).getRunTime()<0 && p.getRunTime()>0)
+			{
+				while(p.getRunTime() > 0) {
+					result.add(p);
+					p.setRunTime(p.getRunTime() - 1);
+					runTime++;
+				}
+			}
+						
+			System.out.println("Total runTime= "+runTime);
+			
+			if(runTime >= 100) {
+				isFull = true;
+			}
+		}
 		
 		
 		return result;
