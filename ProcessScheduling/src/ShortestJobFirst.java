486d9c04b553ab71806d8db54e9af4c3ce2686de
hnjoshi <hnjoshi@live.com>
2015-02-16 01:06:06 -0800

    Shortest Job First Statistics Incomplete
    Unable to trace the wait time of individual process and so avg wait time
    and turnaround time looks impossible to calculate based on current
    implementation.

--- ProcessScheduling/src/ShortestJobFirst.java
+++ ProcessScheduling/src/ShortestJobFirst.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 
 /**
  * This class runs Shortest job First Algorithm.
@@ -9,6 +8,10 @@ import java.util.Comparator;
 public class ShortestJobFirst {
 	
 	Process p = null;
+	int waitTime[], responseTime[];
+	int turnAroundTime[];
+	float avgWaitTime, avgResponseTime, avgTurnaroundTime, throughput;
+	int noOfProcessesExec;
 	int current = 0, runTime = 0;
 	
 	ArrayList<Process> processes; 
@@ -20,6 +23,8 @@ public class ShortestJobFirst {
 	public ArrayList<Process> sjf() {
 		ArrayList<Process> result = new ArrayList<Process>();
 		ArrayList<Process> readyQ = new ArrayList<Process>();
+		waitTime = new int[processes.size()];
+		responseTime = new int[processes.size()];
 		
 		boolean isFull = false;
 		while(!isFull) {
@@ -27,6 +32,7 @@ public class ShortestJobFirst {
 			if(current==0)
 			{
 				p = processes.get(current);
+				responseTime[current] = runTime;
 				while(p.getRunTime() > 0) {
 					result.add(p);
 					p.setRunTime(p.getRunTime() - 1);
@@ -59,7 +65,9 @@ public class ShortestJobFirst {
 							nextPid = j;
 						}
 					}
-				
+
+					responseTime[current] = runTime;
+					
 					//run the readyQ process with nextPid
 					p = readyQ.get(nextPid);
 					while(p.getRunTime() > 0) {
@@ -67,8 +75,9 @@ public class ShortestJobFirst {
 						p.setRunTime(p.getRunTime() - 1);
 						runTime++;
 					}
+					current++;	//counting number of processes executed
 					
-					// Remove same process original processes
+					// Remove same process from original processes
 					for(int j=0; j<processes.size(); j++)
 					{	
 						if(p.getName() == processes.get(j).getName())
@@ -91,6 +100,37 @@ public class ShortestJobFirst {
 			}
 		}
 		
+		//System.out.println("Total processes = "+ current);
+		//System.out.println(Arrays.toString(responseTime));
+		int totalResponse = 0;
+		for(int i=0; i<responseTime.length; i++){
+			totalResponse += responseTime[i];
+		}
+		avgResponseTime = totalResponse/current;
+		System.out.println("avg response time = "+avgResponseTime);
+		throughput = (float)current/runTime;
+		System.out.println("Throughput = "+ throughput);
+		
 		return result;
+	}
+	
+	public float getSJF_avgWaitTime()
+	{
+		return avgWaitTime;
+	}
+	
+	public float getSJF_avgResponseTime()
+	{
+		return avgResponseTime;
+	}
+	
+	public float getSJF_avgTurnaroundTime()
+	{
+		return avgTurnaroundTime;
+	}
+	
+	public float getSJF_throughput()
+	{
+		return throughput;
 	}
 }
